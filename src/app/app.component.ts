f249fbe0381655ab4a8b39aed68ab8db83ea2d7a
Zack Toth <zack@americanweddinggroup.com>
2021-07-08 12:13:57 -0400

    more commits

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Hello WORLD';
+  title = 'Hello WORLD, my -app';
+  description = 'Hello world!'
 }
